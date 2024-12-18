d2b7b1fad17df9f416cd88c728ddf59e8c0d9cfe
szymon cichonski <255574@edu.p.lodz.pl>
2024-12-18 14:53:59 +0100

    master

--- main.c
+++ main.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-int Policz(int a, int b);
 
-int main(void)
-{
+
+int main(void) {
     printf("Hello, World!\n");
-    printf("w pierwszym branchu 2+3=%d\n", Policz(2,3));
     return 0;
-}
-int Policz(int a, int b) {
-    return a + b;
 }
