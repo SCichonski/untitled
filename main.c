27894162f6d3e32fed839d865ce2a1a32ae7bc14
szymon cichonski <255574@edu.p.lodz.pl>
2024-12-18 14:41:14 +0100

    01 Zmiana metody Policz

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+int Policz(int a, int b);
 
 int main(void)
 {
     printf("Hello, World!\n");
+    printf("w pierwszym branchu 2+3=%d\n", Policz(2,3));
     return 0;
+}
+int Policz(int a, int b) {
+    return a + b;
 }
